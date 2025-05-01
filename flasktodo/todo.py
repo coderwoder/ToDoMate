b1f5d8f88e346e96f10f9dc1117e260095d4f55a
Mohammed Ibn Ameen <94846104+coderwoder@users.noreply.github.com>
2025-05-01 19:03:50 +0000

    Created the add function

--- flasktodo/todo.py
+++ flasktodo/todo.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
@@ -7,13 +7,26 @@ app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///data.db"
 
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(150))
+    status=db.Column(db.Boolean)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    todo_lists=Todo.query.all()
+    return render_template("index.html",todo_lists=todo_lists)
 
-# @app.route("/add")
-# def add():
-
+@app.route("/add",methods=["POST"])
+def add():
+    title=request.form.get("title") # Fetch the title
+    new_todo=Todo(title=title) # Make the table, add the values
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home")) # Send (merge) it to the Home page
 
 if __name__ == '__main__':
+    app.app_context().push()
+    db.create_all()
     app.run(debug=True)
+    db.drop_all()
